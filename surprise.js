1eaa745f5606d9e69456008eedaaf8d66e0b55f9
arianeravier <146649076+arianeravier@users.noreply.github.com>
2023-10-01 16:30:17 +0200

    init

---
+++ surprise.js
@@ -0,0 +1,8 @@
+ console.log("hellooo");
+ $(window).on('load', function () {
+    const trucs = document.getElementsByClassName("surp");
+    const val = Math.floor(Math.random()*4)+1;
+    for (var i = 0 ; i < trucs.length ; i++) {
+        trucs[i].classList.add(`surp${val}`);
+    }
+}); 
